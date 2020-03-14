968bcd17bd698234fdd72cee7efbc28f82b59013
Tien Le P <tien.le@asiantech.vn>
2020-03-14 09:54:55 +0700

    overview

--- CombineMVVM/Controllers/WelcomeViewController/WelcomeViewController.swift
+++ CombineMVVM/Controllers/WelcomeViewController/WelcomeViewController.swift
@@ -7,13 +7,55 @@
 //
 
 import UIKit
+import Combine
 
 class WelcomeViewController: UIViewController {
+  
+  //MARK: Outlets
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var aboutLabel: UILabel!
+  @IBOutlet weak var homeButton: UIButton!
+  @IBOutlet weak var loginButton: UIButton!
+  
+  //MARK: Properties
+  let viewModel = WelcomeViewModel(user: .init(name: "Fx", about: "Admin", isLogin: false))
+  
+  var subscriptions = Set<AnyCancellable>()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // title
     title = App.Text.titleWelcomeVC
+    
+    // subscriptions
+    // name
+    viewModel.name
+      .assign(to: \.text, on: nameLabel)
+      .store(in: &subscriptions)
+    
+    // about
+    viewModel.about
+    .assign(to: \.text, on: aboutLabel)
+    .store(in: &subscriptions)
+    
+    // buttons
+    viewModel.loginEnabled
+      .sink { isLogin in
+        self.loginButton.isEnabled = !isLogin
+        self.homeButton.isEnabled = isLogin
+      }
+    .store(in: &subscriptions)
+    
   }
+  
+  //MARK: IBActions
+  @IBAction func loginButtonTourchUpInside(_ sender: Any) {
+    viewModel.action.send(.gotoLogin)
+  }
+  
+  @IBAction func homeButtonTourchUpInside(_ sender: Any) {
+    viewModel.action.send(.gotoHome)
+   }
+  
 }
--- CombineMVVM/Controllers/WelcomeViewController/WelcomeViewModel.swift
+++ CombineMVVM/Controllers/WelcomeViewController/WelcomeViewModel.swift
@@ -10,5 +10,79 @@ import Foundation
 import Combine
 
 final class WelcomeViewModel {
-
+  
+  //MARK: State
+  enum State {
+    case initial
+    case error(message: String)
+  }
+  
+  let state = CurrentValueSubject<State, Never>(.initial)
+  
+  //MARK: Actions
+  enum Action {
+    case gotoLogin
+    case gotoHome
+  }
+  
+  let action = PassthroughSubject<Action, Never>()
+  
+  var subscriptions = Set<AnyCancellable>()
+  
+  //MARK: Properties
+  let name            = CurrentValueSubject<String?, Never>(nil)
+  let about           = CurrentValueSubject<String?, Never>(nil)
+  let loginEnabled    = CurrentValueSubject<Bool, Never>(false)
+  let errorText       = CurrentValueSubject<String?, Never>(nil)
+  
+  var user: User
+  
+  //MARK: init
+  init(user: User) {
+    self.user = user
+    
+    //subscriptions
+    state
+      .sink(receiveValue: { [weak self] state in
+        self?.processState(state)
+      })
+      .store(in: &subscriptions)
+    
+    action
+      .sink(receiveValue: { [weak self] action in
+        self?.processAction(action)
+      })
+      .store(in: &subscriptions)
+  }
+  
+  deinit {
+    subscriptions.removeAll()
+  }
+  
+  //MARK: Private function
+  // STATE
+  private func processState(_ state: State) {
+    switch state {
+    case .initial:
+      name.value = user.name
+      about.value = user.about
+      loginEnabled.value = user.isLogin
+      errorText.value = nil
+      
+    case .error(let message):
+      errorText.value = message
+      
+    }
+  }
+  
+  //ACTION
+  private func processAction(_ action: Action) {
+    switch action {
+    case .gotoHome:
+      print("goto HomeVC")
+    case .gotoLogin:
+      print("goto LoginVC")
+    }
+  }
+  
 }
